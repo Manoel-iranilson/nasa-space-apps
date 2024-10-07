a329080eab361a3a0678be6366c4c162846c1a46
manoel-iranilson <manoelvieirafilho0@gmail.com>
2024-10-06 22:25:59 -0300

    feat: project

---
+++ src/app/components/planetModal/index.tsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useRef } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Grid,
+  Box,
+} from "@chakra-ui/react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+
+// Componente para a esfera com textura
+function SphereWithTexture({ position, texturePath, args }: any) {
+  const texture = useLoader(THREE.TextureLoader, texturePath);
+  const meshRef = useRef();
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(1, 1);
+  useFrame((state, delta) => (meshRef.current.rotation.y += delta));
+
+  return (
+    <mesh ref={meshRef} position={position}>
+      <sphereGeometry args={args} />
+      <meshStandardMaterial map={texture} />
+    </mesh>
+  );
+}
+
+// Componente do modal do planeta
+const PlanetModal = ({ isOpen, onClose, planetData }: any) => {
+  if (!planetData) return null;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay
+        bg="blackAlpha.800" // Fundo preto com opacidade
+        backdropFilter="blur(10px)" // Efeito de desfoque no fundo
+      />
+      <ModalContent
+        maxW="600px" // Largura máxima do modal
+        bg="gray.900" // Fundo cinza escuro
+        color="white" // Texto branco
+        borderRadius="lg"
+        gap={20}
+        boxShadow="lg"
+      >
+        <ModalHeader textAlign="center" fontSize="2xl" fontWeight="bold">
+          {planetData.name}
+        </ModalHeader>
+        <ModalBody>
+          <Grid templateColumns="1fr 1fr" gap={5}>
+            <Box p={4} className="w-96">
+              <h2 className="text-3xl font-semibold pb-4">
+                {planetData.pl_name}
+              </h2>
+              <p className="text-gray-300 text-lg">{planetData.description}</p>
+              <p className="text-gray-400 text-lg">
+                Distância do Sol: {planetData.distance} unidades
+              </p>
+              <p className="text-gray-400 text-lg">
+                Tamanho: {planetData.size} vezes a Terra
+              </p>
+              <p className="text-gray-400 text-lg">
+                Data de descoberta: {planetData.disc_year}
+              </p>
+              <p className="text-gray-400 text-lg">
+                Telescopio que foi descoberto: {planetData.disc_telescope}
+              </p>
+            </Box>
+
+            {/* Planeta 3D */}
+            <Box>
+              <Canvas style={{ height: "500px" }}>
+                <ambientLight intensity={2} />
+                <SphereWithTexture
+                  args={[2, 32, 32]} // Tamanho do planeta
+                  position={[0, 0, 0]}
+                  scale
+                  texturePath={planetData.texturePath}
+                />
+              </Canvas>
+            </Box>
+          </Grid>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" onClick={onClose}>
+            Fechar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default PlanetModal;
---
+++ src/app/components/starMap/index.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { Physics } from "@react-three/cannon";
+import { animated, useSpring } from "@react-spring/three";
+import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import PlanetModal from "../planetModal";
+import { planets } from "@/app/constants/planets";
+
+function SphereWithTexture({
+  position,
+  texturePath,
+  onClick,
+  rotationSpeed = 0.01,
+}) {
+  const texture = useLoader(THREE.TextureLoader, texturePath);
+  const meshRef = useRef();
+
+  // Adiciona rotação contínua
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += rotationSpeed; // Ajuste a velocidade da rotação
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} position={position} onClick={onClick}>
+      <sphereGeometry args={[1, 32, 32]} /> {/* Ajustar raio se necessário */}
+      <meshStandardMaterial map={texture} />
+    </mesh>
+  );
+}
+
+export default function StarMap() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+  const [targetPosition, setTargetPosition] = useState([0, 0, 0]);
+
+  // Animação suave para a posição da câmera
+  const { cameraPosition } = useSpring({
+    cameraPosition: targetPosition,
+    config: { mass: 1, tension: 170, friction: 26 }, // Ajuste da suavidade
+  });
+
+  // Texturas dos planetas
+  const planetTextures = ["/terra.webp", "/planetaAzul.png", "/marte.png"];
+
+  const openModal = (planet, position) => {
+    setTargetPosition(position);
+    setTimeout(() => {
+      setSelectedPlanet(planet);
+      setIsModalOpen(true);
+    }, 500);
+  };
+
+  return (
+    <div className="bg-black h-screen flex justify-center items-center">
+      <Canvas>
+        <animated.perspectiveCamera position={cameraPosition}>
+          <OrbitControls />
+          <ambientLight intensity={2} />
+          <Stars />
+
+          <Physics>
+            <SphereWithTexture
+              position={[0, 0, 0]} // O Sol está no centro
+              texturePath="/sol.png"
+              rotationSpeed={0.002} // Ajuste a velocidade de rotação do Sol
+              onClick={() =>
+                openModal({
+                  name: "Sol",
+                  description: "O Sol é a estrela central do Sistema Solar.",
+                  size: 5, // Tamanho do Sol
+                  texturePath: "/sol.png",
+                })
+              }
+            />
+
+            {/* Planetas ao redor do Sol */}
+            {planets.map((planet, index) => {
+              const distance = 10 + index * 5; // Distância do Sol (aumenta para cada planeta)
+              const angle = (index * (Math.PI / 4)) % (2 * Math.PI); // Ângulo para posicionar os planetas em uma órbita circular
+
+              // Calcula as coordenadas X e Z para cada planeta
+              const posX = distance * Math.cos(angle);
+              const posZ = distance * Math.sin(angle);
+
+              return (
+                <SphereWithTexture
+                  key={planet.pl_name}
+                  position={[posX, 0, posZ]} // Planeta posicionado ao redor do Sol
+                  texturePath={planetTextures[index % planetTextures.length]}
+                  rotationSpeed={0.01} // Ajuste a velocidade de rotação de cada planeta
+                  onClick={() =>
+                    openModal(
+                      {
+                        ...planet,
+                        texturePath:
+                          planetTextures[index % planetTextures.length],
+                      },
+                      [-posX, 0, -posZ]
+                    )
+                  }
+                />
+              );
+            })}
+          </Physics>
+        </animated.perspectiveCamera>
+      </Canvas>
+
+      <PlanetModal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+          setTimeout(() => {
+            setTargetPosition([0, 0, 0]);
+          }, 200);
+        }}
+        planetData={selectedPlanet}
+      />
+    </div>
+  );
+}
